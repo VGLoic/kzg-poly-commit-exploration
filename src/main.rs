bc48287326d060aab626e918c0fd678a073d353a
Loïc Vincent-Genod <37535451+VGLoic@users.noreply.github.com>
2025-07-10 18:42:13 +0200

    setup basic logging and env file (#3)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    if let Err(err) = dotenvy::dotenv() {
+        if !err.not_found() {
+            panic!("Error while loading .env file: {err}")
+        }
+    }
+
+    let logger_env = env_logger::Env::default()
+        .filter_or("LOG_LEVEL", "debug")
+        .write_style_or("LOG_STYLE", "always");
+    env_logger::init_from_env(logger_env);
+
+    log::info!("Hello, world!");
 }
