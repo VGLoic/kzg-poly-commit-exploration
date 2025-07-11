c4220d986a21eceebca31651f715cb607007efea
VGLoic <vincent.genod.loic@gmail.com>
2025-07-11 16:28:47 +0200

    load .env before reading env, correct typo

--- src/main.rs
+++ src/main.rs
@@ -28,19 +28,20 @@ fn main() {
         _ => log::Level::Trace,
     };
 
-    let log_level = match std::env::var("LOG_LEVEL").ok() {
-        Some(v) => log::Level::from_str(v.as_str()).unwrap_or(default_log_level),
-        None => default_log_level,
-    };
-
     if let Err(err) = dotenvy::dotenv() {
         if !err.not_found() {
             panic!("Error while loading .env file: {err}")
         }
     }
 
+    let log_level = match std::env::var("LOG_LEVEL").ok() {
+        Some(v) => log::Level::from_str(v.as_str()).unwrap_or(default_log_level),
+        None => default_log_level,
+    };
+
+
     if let Err(err) = simple_logger::init_with_level(log_level) {
-        panic!("Fail to initialize logging, got error: {err}");
+        panic!("Failed to initialize logging, got error: {err}");
     }
 
     match &cli.command {
